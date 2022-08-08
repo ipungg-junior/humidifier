eeb199206755ec9cb5c38eacf74b1cada5858f3e
ipungg-junior <ipungg.id@gmail.com>
2022-08-08 16:06:58 +0700

    Create new app 'apps'

---
+++ esp_simulator/esp.py
@@ -0,0 +1,58 @@
+from itertools import count
+import requests,time, random, json, sys
+
+
+deviceID = sys.argv[1]
+urlNewSession = 'http://127.0.0.1:8000/service/register-session/'
+urlPushData = 'http://127.0.0.1:8000/service/post/'
+
+ 
+
+def publishData(sessionID, code):
+    temp1 = random.randint(28, 36)
+    temp2 = random.randint(0, 3)
+    temp3 = random.randint(0, 1)
+    myobj = {
+    'deviceID':deviceID,
+    'suhu_chamber_a': str(temp1),
+    'suhu_chamber_b': str(temp1+temp2),
+    'suhu_output_pasien': str(temp1-temp3),
+    'suhu_heater_plate':str(temp1+temp3),
+    'arus_heater_plate':str(temp1+temp2),
+    'arus_heater_wire':str(temp1+temp3+1),
+    'code' : str(code),
+    }
+    req = requests.post(urlPushData, json=myobj, headers={"Session-ID": sessionID})
+    print(req.status_code)
+
+
+def register(deviceID):
+    requestSession = {
+        'deviceID':deviceID
+    }
+
+    req = requests.post(urlNewSession, json=requestSession)
+    if req.status_code == 200:
+        data = json.loads(req.text)
+        return data
+    else:
+        # Register fail!
+        return None
+
+
+time.sleep(1)
+
+if __name__ == '__main__':
+    response = register(deviceID)
+    counter = 7
+    while True:
+        if (counter==3):
+            publishData(response['sessionID'], 1)
+        else:
+            publishData(response['sessionID'], 0)
+        
+        if (counter==0):
+            counter=7
+        else:
+            counter -= 1
+        time.sleep(1)
