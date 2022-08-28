b2fec3f420725bade8955752fd1372cbc25cda77
ipungg-junior <ipungg.id@gmail.com>
2022-08-28 21:50:21 +0700

    add static file & fix fontasewome

---
+++ static/js/register-device.js
@@ -0,0 +1,98 @@
+var openModalBtn = document.getElementById("add-device-item");
+var modal = document.getElementById("main-modal");
+var inputDeviceID = document.getElementById("id_deviceID");
+var inputDeviceRoom = document.getElementById("id_deviceRoom");
+var submitModalBtn = document.getElementById("modal-btn-submit");
+var badgeInfo = document.getElementById("badge-info");
+var badgeInfoText = document.getElementById("badge-info-text");
+var tmpResp = ''
+
+function preLoaderPage(){
+    inputDeviceID.value = '';
+    inputDeviceRoom.value = '';
+}
+
+function reload(){
+    window.location.href = '/monitoring/';
+}
+
+function disableModal(){
+    submitModalBtn.value = 'Loading..';
+    submitModalBtn.disabled = true;
+    submitModalBtn.style.backgroundColor = 'gray';
+}
+
+function enableModal(){
+    submitModalBtn.value = 'Submit';
+    submitModalBtn.disabled = false;
+    submitModalBtn.style.backgroundColor = 'rgb(17, 163, 156)';
+    inputDeviceID.value = '';
+    inputDeviceRoom.value = '';
+}
+
+// Fungsi untuk register device baru
+function register(deviceID, deviceRoom) {
+    $.ajax({
+        type: "POST",
+        url: "/service/register/",
+        headers: {
+            "X-CSRFToken": "{{csrf_token}}",
+            "Content-Type": "application/json",
+        },
+        data: {
+            "deviceID": deviceID,
+            "deviceRoom": deviceRoom,
+        },
+        success: function (data) {
+            tmpResp = (JSON.parse(data));
+            
+
+            switch (tmpResp['flag']){
+                case 'danger':
+                    // badgeInfo.style.backgroundColor = 'rgb(179, 52, 6)';
+                    alert('[ERROR] : ' + tmpResp['message']);
+                    break;
+                case 'warning':
+                    // badgeInfo.style.backgroundColor = 'rgb(170, 152, 14)';
+                    alert('[WARNING] : ' + tmpResp['message']);
+                    break;
+                case 'info':
+                    // badgeInfo.style.backgroundColor = 'rgb(17, 163, 156)';
+                    alert(tmpResp['message'] + ' Mohon Tunggu....');
+                    window.setTimeout(reload, 2500);
+                    break;
+            }
+
+            // modal.style.display = 'none';
+            // badgeInfoText.textContent = tmpResp['message'];
+            // badgeInfo.style.display = 'initial';
+            
+            
+        },
+        failure: function (data) {
+            // badgeInfo.style.backgroundColor = 'red';
+            // badgeInfoText.textContent = 'Terdapat kesalahan jaringan ...';
+            // badgeInfo.style.display = 'initial';
+        },
+        
+    });
+    enableModal();
+}
+
+submitModalBtn.onclick = function (){
+    disableModal();
+    register(inputDeviceID.value, inputDeviceRoom.value);
+}
+
+openModalBtn.onclick = function () {
+    modal.style.display = "block";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Call init function
+preLoaderPage();
---
+++ staticfiles/js/device-post.js
@@ -0,0 +1,66 @@
+
+let incomingDeviceData = {};
+var all_websocket = []
+// Konek ke setiap device yang ada (websocket)
+for(let i=0;i<device_timelapse.length; i++){
+    all_websocket[i] = new WebSocket('ws://' + window.location.host + '/streaming/' + device_timelapse[i]['id'] + '/');
+    all_websocket[i].onopen = function (e) {console.log("connect " + device_timelapse[i]['id']);}
+    all_websocket[i].onclose = function (e) {console.log("connect " + device_timelapse[i]['id']);}
+
+    // Websocket menerima message
+    all_websocket[i].onmessage = function (e) {
+        incomingDeviceData = (JSON.parse(e.data))['data'];
+    
+        // update data on webpage meneyesuaikan element css nya
+        for(let i=0;i<device_timelapse.length; i++){
+            let device = device_timelapse[i];
+
+
+            if (device['id']==incomingDeviceData['deviceID']){
+                changeTemp(incomingDeviceData['deviceID']);
+                device['timelive'] = 3;
+
+            }
+
+        }
+    }   
+}
+
+
+function changeTemp(deviceID){
+    document.getElementById('temp-chamber-'+deviceID).textContent = incomingDeviceData['suhu_chamber_a'];
+}
+
+function disconnectedDevice(deviceID){
+    document.getElementById('card-'+deviceID).classList.remove('active');
+    document.getElementById('card-'+deviceID).classList.add('deactivate');
+
+    report(deviceID);
+}
+
+function connectedDevice(deviceID){
+    document.getElementById('card-'+deviceID).classList.remove('deactivate');
+    document.getElementById('card-'+deviceID).classList.add('active');
+}
+
+
+
+
+// Fungsi timer untuk mengganti warga background card jika koneksi terputus
+var countdownTime = setInterval(function(){
+        for(let i=0;i<device_timelapse.length; i++){
+          let device = device_timelapse[i];
+          if (device['timelive'] == 0){
+            if (device['status']=='True'){     
+                disconnectedDevice(device['id']);
+                device['status'] = 'False';
+            }
+          }else{
+            if (device['status']=='False'){    
+                connectedDevice(device['id'])
+                device['status'] = 'True';
+            }
+                device['timelive'] -= 1;
+          }
+        }
+  }, 1000);
