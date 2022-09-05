57100c7e72ada99df6d0aefa5f8c738a00a61775
ipungg-junior <ipungg.id@gmail.com>
2022-09-05 16:09:39 +0700

    update ino file

--- ESP_MJM/NEW_ESP32.ino
+++ ESP_MJM/NEW_ESP32.ino
@@ -5,7 +5,7 @@
 #include <ArduinoJson.h>
 
 #define USE_SERIAL Serial
-String mainUrl = "http://192.168.111.64:8000/";
+String mainUrl = "http://0.tcp.ap.ngrok.io:12928/";
 String URL_REGISTER_DEVICE = "service/register-machine-code/";
 String URL_SESSION = "service/register-session/";
 String URL_PUBLISH = "service/publish/";
@@ -14,7 +14,11 @@ String sessionID, deviceID;
 WiFiMulti wifiMulti;
 HTTPClient http;
 
-void setup() {
+int chamberA, chamberB, suhuPasien, suhuPlate, arusPlate, arusWire, code;
+
+
+void setup()
+{
 
   USE_SERIAL.begin(9600);
 
@@ -22,7 +26,8 @@ void setup() {
   USE_SERIAL.println();
   USE_SERIAL.println();
 
-  for (uint8_t t = 4; t > 0; t--) {
+  for (uint8_t t = 4; t > 0; t--)
+  {
     USE_SERIAL.printf("[SETUP] WAIT %d...\n", t);
     USE_SERIAL.flush();
     delay(1000);
@@ -30,78 +35,99 @@ void setup() {
 
   wifiMulti.addAP("realme 9 Pro+", "11111111");
 
+  deviceID = "10107";
+  sessionID = getSession();
 }
 
-void loop() {
+void loop()
+{
 
+  if ((wifiMulti.run() == WL_CONNECTED))
+  {
 
-  if ((wifiMulti.run() == WL_CONNECTED)) {
-
-    if (deviceID == ""){
-      deviceID = getDeviceID();
-      sessionID = getSession();
-      Serial.println(deviceID + sessionID);
-    }
-
+    Serial.println(deviceID + sessionID);
+    int statusCode = publishData();
 
     delay(5000);
   }
-
 }
 
-
-String getSession() {
+String getSession()
+{
   String input = "{\"deviceID\":" + deviceID + "}";
   const size_t capacity = JSON_OBJECT_SIZE(3) + JSON_ARRAY_SIZE(2) + 60;
   DynamicJsonDocument doc(capacity);
-  deserializeJson(doc, input);  
+  deserializeJson(doc, input);
   JsonObject obj = doc.as<JsonObject>();
-  
-  http.begin(mainUrl);
-  int httpCode = http.POST((doc.as<String>()));
-  
+
+  Serial.print("Request session for device : ");
+  Serial.println(deviceID);
+  Serial.print("Body : ");
+  Serial.println(doc.as<String>());
+  http.begin(mainUrl + URL_SESSION);
+  int httpCode = http.POST(input);
+  Serial.print("Session response code : ");
+  Serial.println(httpCode);
   String payload = http.getString();
   DeserializationError error = deserializeJson(doc, payload);
-  if (error) {
-    Serial.print(F("deserializeJson() failed: "));
-    Serial.println(error.f_str());
-    http.end();
-    return;
-  }
+  http.end();
 
   // Extract values
   return (doc["sessionID"].as<String>());
 }
 
+int publishData()
+{
+  String input = "{\"deviceID\":" + deviceID + "," + 
+                 "\"sessionID\":" + sessionID + "," +
+                 "\"suhu_chamber_a\":" + chamberA + "," +
+                 "\"suhu_chamber_b\":" + chamberB + "," +
+                 "\"suhu_output_pasien\":" + suhuPasien + "," +
+                 "\"suhu_heater_plate\":" + suhuPlate + "," +
+                 "\"arus_heater_plate\":" + arusPlate + "," +
+                 "\"arus_heater_wire\":" + arusWire + "," +
+                 "\"code\":" + code + "}";
+  const size_t capacity = JSON_OBJECT_SIZE(3) + JSON_ARRAY_SIZE(2) + 60;
+  DynamicJsonDocument doc(capacity);
+  deserializeJson(doc, input);
+  JsonObject obj = doc.as<JsonObject>();
 
-int publishData() {
+  http.begin(mainUrl + URL_PUBLISH);
+  int httpCode = http.POST(input);
 
+  String payload = http.getString();
+  http.end();
+  return httpCode;
 }
 
-
-String getDeviceID() {
-  http.begin(mainUrl + URL_REGISTER_DEVICE); //HTTP
+String getDeviceID()
+{
+  http.begin(mainUrl + URL_REGISTER_DEVICE); // HTTP
   int httpCode = http.GET();
 
-  if (httpCode > 0) {
+  if (httpCode > 0)
+  {
 
     // file found at server
-    if (httpCode == HTTP_CODE_OK) {
+    if (httpCode == HTTP_CODE_OK)
+    {
       String payload = http.getString();
       const size_t capacity = JSON_OBJECT_SIZE(3) + JSON_ARRAY_SIZE(2) + 60;
       DynamicJsonDocument doc(capacity);
       DeserializationError error = deserializeJson(doc, payload);
-      if (error) {
+      if (error)
+      {
         Serial.print(F("deserializeJson() failed: "));
         Serial.println(error.f_str());
         http.end();
       }
       deviceID = (doc["machine-code"].as<String>());
     }
-  } else {
+  }
+  else
+  {
     USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
   }
 
   http.end();
 }
-
