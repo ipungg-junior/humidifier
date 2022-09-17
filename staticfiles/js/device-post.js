375efaef5b52b30a5f13ca820e684a370a9c2e8f
ipungg-junior <ipungg.id@gmail.com>
2022-09-18 01:09:46 +0700

    Javascript counter if device disconnected on device-post.js

--- staticfiles/js/device-post.js
+++ staticfiles/js/device-post.js
@@ -1,12 +1,16 @@
 
 let incomingDeviceData = {};
 var all_websocket = []
-
+ 
 // Konek ke setiap device yang ada (websocket)
 for(let i=0;i<device_timelapse.length; i++){
-    all_websocket[i] = new WebSocket('wss://' + window.location.host + '/streaming/' + device_timelapse[i]['id'] + '/');
-    all_websocket[i].onopen = function (e) {console.log("connect " + device_timelapse[i]['id']);}
-    all_websocket[i].onclose = function (e) {console.log("connect " + device_timelapse[i]['id']);}
+    all_websocket[i] = new WebSocket('ws://' + window.location.host + '/streaming/' + device_timelapse[i]['id'] + '/');
+    all_websocket[i].onopen = function (e) {
+        console.log("WS connected " + device_timelapse[i]['id']);
+    }
+    all_websocket[i].onclose = function (e) {
+        console.log("WS connection closed from " + device_timelapse[i]['id']);
+    }
 
     // Websocket menerima message
     all_websocket[i].onmessage = function (e) {
@@ -19,7 +23,7 @@ for(let i=0;i<device_timelapse.length; i++){
 
             if (device['id']==incomingDeviceData['deviceID']){
                 changeTemp(incomingDeviceData['deviceID']);
-                device['timelive'] = 10;
+                device['timelive'] = 6;
 
             }
 
@@ -36,42 +40,56 @@ function disconnectedDevice(deviceID){
     document.getElementById('card-'+deviceID).classList.remove('active');
     document.getElementById('card-'+deviceID).classList.add('deactivate');
 
-    report(deviceID);
+    //trace device on array
+    device_timelapse.forEach(device => {
+        //  jika device ditemukan maka ubah statusnya ke false lalu kirim sinyal disconnect
+        if (device['id'] == deviceID){
+            device['status'] = 'False';
+            report(deviceID);
+        }
+    });
+    
 }
 
 function connectedDevice(deviceID){
     document.getElementById('card-'+deviceID).classList.remove('deactivate');
     document.getElementById('card-'+deviceID).classList.add('active');
+
+    console.log('change ' + deviceID + 'status True');
+    //trace device on array
+    device_timelapse.forEach(device => {
+        //  jika device ditemukan maka ubah statusnya ke false lalu kirim sinyal disconnect
+        if (device['id'] == deviceID){
+            device['status'] = 'True';
+        }
+    });
 }
 
 
 
 
-// Fungsi timer untuk mengganti warga background card jika koneksi terputus
+// Fungsi timer untuk memberi sinyal kepada front-end jika koneksi http dari device terputus (setiap 1 detik)
 var countdownTime = setInterval(function(){
-        for(let i=0;i<device_timelapse.length; i++){
-          let device = device_timelapse[i];
 
-          // timelive = 0
-          if (device['timelive'] == 0){
+        device_timelapse.forEach(device => {
 
-            if (device['status']=='True'){     
-                disconnectedDevice(device['id']);
-                device['status'] = 'False';
-            }
+            // jika timelive device variable sudah 0, artinya koneksi yg ditunggu selama 10 detik tidak datang
+            // maka akan kirim sinyal disconnect ke server
+            if (device['timelive'] == 0) {
 
-          }
+                if (device['status'] == 'True'){
+                    disconnectedDevice(device['id']);
+                }
 
-          // timelive > 0
-          else{
+            }else{
 
-            if (device['status']=='False'){    
-                connectedDevice(device['id'])
-                device['status'] = 'True';
-            }
+                if (device['status'] == 'False'){
+                    connectedDevice(device['id']);
+                }
+
+                //kurangi timelive device setiap 1 detiknya
                 device['timelive'] -= 1;
+            }
+        });
 
-          }
-        }
   }, 1000);
-
