fb312d85372375819c714210fb6cc3dda929fd00
ipungg-junior <ipungg.id@gmail.com>
2022-09-23 22:27:15 +0700

    check bash

--- git.sh
+++ git.sh
@@ -1,14 +1,32 @@
-while getopts m: flag
-do
-    case "${flag}" in
-        m) message=${OPTARG};;
-    esac
-done
-git add .
-echo "Add to local branch completed . . .";
-sleep 1
-git commit -m "$message"
-echo "Commit to local branch with message : $message"
-echo "Commit completed . . ."
-sleep 2
-cls
+#!/bin/bash
+git status
+
+echo " "
+read -p "Do you want check pull sync with master branch ? (y/n) " yn
+case "$yn" in
+
+y) 
+	git pull origin master;
+	sleep 1
+	read -p "Do you want force push? (y for push or N for commit local) " yN
+	case "$yN" in
+	        y)
+	        	read -p "Input Message push repo : " m
+			git add .;
+			echo "Add to local branch completed . . ."
+			echo ""
+			echo "Commit to local branch with message : $m"
+			git commit -m "$m"
+			echo "Commit completed . . ."
+			sleep 3
+			clear;;
+	        n) 
+	        	clear
+	esac;;
+
+n) 
+	exit;;
+esac
+
+
+
