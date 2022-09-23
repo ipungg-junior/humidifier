84dd95e1bd235c7865fb3f510076aa1e341bfa92
ipungg-junior <ipungg.id@gmail.com>
2022-09-23 22:28:43 +0700

    check bash v2

--- git.sh
+++ git.sh
@@ -6,17 +6,20 @@ read -p "Do you want check pull sync with master branch ? (y/n) " yn
 case "$yn" in
 
 y) 
+	clear
 	git pull origin master;
 	sleep 1
 	read -p "Do you want force push? (y for push or N for commit local) " yN
 	case "$yN" in
 	        y)
+	        	clear
 	        	read -p "Input Message push repo : " m
 			git add .;
 			echo "Add to local branch completed . . ."
 			echo ""
 			echo "Commit to local branch with message : $m"
 			git commit -m "$m"
+			echo ""
 			echo "Commit completed . . ."
 			sleep 3
 			clear;;
